40cd3f0f6e9adf8ba4382f220c9f1913b82cb74f
Cody Cai <cai0754@163.com>
2022-05-23 10:18:18 +0800

    Update settings.py

--- firstApp/settings.py
+++ firstApp/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gq#3q9lb)ii5pm@3f0o(v%fa^hr)k6@o7^bt$&5w=%q*hh5^1('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ["111.231.13.100","127.0.0.1"]
+ALLOWED_HOSTS =  ["127.0.0.1"]
 
 
 # Application definition
